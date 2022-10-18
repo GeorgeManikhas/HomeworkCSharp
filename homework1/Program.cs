a54dfdf9f381de29ad777aa4441124a9af399b44
GeorgeM <gemanikhas@mail.ru>
2022-10-19 02:08:27 +0300

    1onedone

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,4 +1,26 @@
 ﻿// Домашнее задание 
-// Задача 1.
-// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Задача 2.
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньше
 
+
+/*
+
+Console.Write("Input the first number:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the second number:  ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+    Console.Write(a + " is the biggest number and " + b + " is the smallest one");
+}
+else
+{
+    Console.Write(b + " is the biggest number and " + a + " is the smallest one");
+}
+
+*/
+
+
+// Задача 4.
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
