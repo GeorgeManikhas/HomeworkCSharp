714f9cecb90750dd232ae8db3e279132ce373f0b
GeorgeM <gemanikhas@mail.ru>
2022-10-19 23:56:11 +0300

    10thdone

---
+++ homework2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10.
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+/*
+
+int NewNumber(int number)
+{
+    number = number / 10;
+    number = number % 10;
+    return number;
+}
+
+Console.Write("Input a three digit number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int result = NewNumber(a);
+
+Console.WriteLine("Your updated number of " + a + " is " + result);
+
+*/
